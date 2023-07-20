324296dc5c8a703b5e5515531bbff63a75850c25
minjo-on <103498687+minjo-on@users.noreply.github.com>
2023-07-20 21:18:46 +0900

    Update accounts.ts
    domain = nninjo-on
    name = samsung

--- cdk/lib/config/accounts.ts
+++ cdk/lib/config/accounts.ts
@@ -8,7 +8,7 @@ export interface Account {
 export const Accounts: Account[] = [
     {
         accountId: '343102482966',
-        stage: 'nninjo-on',
+        stage: 'samsung',
         region: 'ap-northeast-2',
         airportCode: 'ICN',
     },
