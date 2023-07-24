8fea177b9e6aec7032b5ca5e4cb8f7699aa12738
minjo-on <103498687+minjo-on@users.noreply.github.com>
2023-07-25 01:16:24 +0900

    Update r_csv.py
    csv->dynamodb 형식으로 변경

--- app/backend/r_csv/r_csv.py
+++ app/backend/r_csv/r_csv.py
@@ -1,21 +1,21 @@
-
 import boto3
-import pandas as pd
-import io
-
-bucket_name = 'samsung-icn-moneybook-bucket'  # S3 버킷 이름
-csv_file_name = 'moneybook.csv'  # 가져올 파일 이름
+from boto3.dynamodb.conditions import Key
 
 
 def lambda_handler(event, context):
+    
+   #이벤트 데이터 받아오기
+   user = event['User']
+   
+   #AWS SDK 클라이언트 생성
+   dynamodb = boto3.resource('dynamodb')
+    
+   #db table 정보
+   table = dynamodb.Table('Moneybook')
 
-    #s3 버킷에서 csv 데이터 가져오기
-    s3_client = boto3.client("s3")
-    response = s3_client.get_object(Bucket=bucket_name,Key=csv_file_name)
-    data = response["Body"].read().decode("utf-8")
-
-    #데이터 프레임으로 변경, 빈칸은 0으로 채우고 전달
-    data = pd.read_csv(io.StringIO(data))
-
-    return data.to_json(orient='records')
+   #해당 USER 조회 후 데이터 변수에 저장
+   response = table.query(KeyConditionExpression=Key('User').eq(user))   
+   items = response['Items']
+   
+   return items
 
