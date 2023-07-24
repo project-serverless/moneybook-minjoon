95f3f56a65ab5e21800c64fde941a727a37b6d8a
minjo-on <103498687+minjo-on@users.noreply.github.com>
2023-07-25 01:16:20 +0900

    Update d_csv.py
    csv->dynamodb 형식으로 변경

--- app/backend/d_csv/d_csv.py
+++ app/backend/d_csv/d_csv.py
@@ -1,33 +1,36 @@
 import boto3
-import os
-import io
-import pandas as pd
+from boto3.dynamodb.conditions import Key
 
 bucket_name = 'samsung-icn-moneybook-bucket'  # S3 버킷 이름
 csv_file_name = 'moneybook.csv'  # 가져올 파일 이름
 
 def lambda_handler(event, context):
 
-    #삭제할 인덱스 데이터 가져오기
-    delete_index = event['delete_index']
+    #받아온 데이터
+    user = event['User']
+    date = event['Date']
 
-    #s3 버킷에서 csv 데이터 가져오기
-    s3_client = boto3.client("s3")
-    response = s3_client.get_object(Bucket=bucket_name,Key=csv_file_name)
-    csv_data = response["Body"].read().decode("utf-8")
-
-    #csv 데이터를 데이터프레임으로 변환
-    data = pd.read_csv(io.StringIO(csv_data))
-
-    #해당 인덱스 삭제
-    data.drop(delete_index-1,inplace=True)
+    #AWS SDK 클라이언트 생성
+    dynamodb = boto3.resource('dynamodb')
     
-    #csv 데이터로 변환
-    csv_data = data.to_csv(index=False, encoding='utf-8')
+    #db 테이블 정보
+    table = dynamodb.Table('Moneybook')
+
+    try:
+        table.delete_item(Key={'User':user,'Date':date})
+
+    except Exception as e:
+        # 업데이트 실패시 예외 처리
+        response = table.scan()
+        items = response['Items']
+        return {
+            'statusCode': 500,
+            'body': str(e)
+        }
     
-    #s3 버켓에 파일 덮어쓰기
-    s3_client.put_object(Bucket = bucket_name, Key=csv_file_name,Body=csv_data)
- 
-    return data.to_json(orient='records')
+    else :
+        response = table.query(KeyConditionExpression=Key('User').eq(user))
+        items = response['Items']
+        return items
 
 
